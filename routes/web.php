3dc01e438d728559a9e46cb9772710ff7cd3060c
ditorbejo <ditoooriq@gmail.com>
2021-09-13 12:20:01 +0700

    Membuat authentikasi admin dengan laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,36 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PDFController;
 use App\Http\Controllers\ReportController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('create-surat');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('admin/list-cetak', function () {
     return view('antri-cetak');
-})->name('list-cetak');
+})->middleware(['auth'])->name('list-cetak');
 
 Route::get('admin/data-warga', function () {
     return view('data-warga');
-});
+})->middleware(['auth'])->name('data-warga');
 
 Route::get('admin/data-kelurahan', function () {
     return view('data-kelurahan');
-});
+})->middleware(['auth'])->name('data-kelurahan');
 
 Route::get('admin', function () {
     return redirect()->route('list-cetak');
@@ -28,4 +43,7 @@ Route::get('test', [PDFController::class, 'create']);
 
 Route::get('cetak-surat/{nik}', [PDFController::class, 'index']);
 
-Route::get('admin/buku-register', [ReportController::class, 'render']);
+Route::get('admin/buku-register', [ReportController::class, 'render'])->middleware(['auth'])->name('buku-register');
+
+require __DIR__.'/auth.php';
+
